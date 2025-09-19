769a58d7d1ab1393855a4b08433b1490c0933308
Omena0 <omena0mc@gmail.com>
2025-09-19 11:32:29 +0000

    7C - Tests & remove unnecessary submodules

--- engine/__init__.py
+++ engine/__init__.py
@@ -16,3 +16,26 @@ from . import text
 from . import util
 from . import theme
 
+
+__all__ = [
+    'Frame',
+    'Button',
+    'Label',
+    'Field',
+    'CheckBox',
+    'Toggle',
+    'Dropdown',
+    'Slider',
+    'Radio',
+    'ProgressBar',
+    'IconButton',
+    'SegmentedButton',
+    'TabFrame',
+    'Image',
+    'ChildWindow',
+    'Window',
+    'InputManager',
+    'text',
+    'util',
+    'theme'
+]
--- engine/components/__init__.py
+++ engine/components/__init__.py
@@ -1,16 +1,34 @@
-from .frame import *
-from .button import *
-from .label import *
-from .field import *
-from .checkbox import *
-from .toggle import *
-from .dropdown import *
-from .slider import *
-from .radio import *
-from .progress import *
-from .iconbutton import *
-from .segmented import *
-from .tabframe import *
-from .image import *
-from .childwindow import *
+from .frame import Frame
+from .button import Button
+from .label import Label
+from .field import Field
+from .checkbox import CheckBox
+from .toggle import Toggle
+from .dropdown import Dropdown
+from .slider import Slider
+from .radio import Radio
+from .progress import ProgressBar
+from .iconbutton import IconButton
+from .segmented import SegmentedButton
+from .tabframe import TabFrame
+from .image import Image
+from .childwindow import ChildWindow
 
+
+__all__ = [
+    'Frame',
+    'Button',
+    'Label',
+    'Field',
+    'CheckBox',
+    'Toggle',
+    'Dropdown',
+    'Slider',
+    'Radio',
+    'ProgressBar',
+    'IconButton',
+    'SegmentedButton',
+    'TabFrame',
+    'Image',
+    'ChildWindow'
+]
